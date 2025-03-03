500dbbbae5f1f23515e393469f23a383aae6654b
Krutz <the1Krutz@gmail.com>
2025-03-03 15:13:43 -0500

    switch to expressjs

--- scaffold.sh
+++
@@ -1,6 +0,0 @@
-dotnet new sln -n Gwn
-dotnet new webapi -o Gwn.Service -f "net8.0" -controllers
-dotnet sln Gwn.sln add Gwn.Service/Gwn.Service.csproj
-
-dotnet new classlib -f "net8.0" -o Cities.Lib
-dotnet sln Gwn.sln add Cities.Lib/Cities.Lib.csproj
